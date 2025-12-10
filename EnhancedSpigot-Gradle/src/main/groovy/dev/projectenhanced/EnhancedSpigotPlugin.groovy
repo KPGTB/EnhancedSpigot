bbf62929c0cfd5a14ef3925cd6e9731bd2f0d0cb
KPGTB <kok.krzysztof@outlook.com>
2025-12-10 19:40:58 +0100

    1.0.1 plugin

--- EnhancedSpigot-Gradle/src/main/groovy/dev/projectenhanced/EnhancedSpigotPlugin.groovy
+++ EnhancedSpigot-Gradle/src/main/groovy/dev/projectenhanced/EnhancedSpigotPlugin.groovy
@@ -1,25 +1,41 @@
+/*
+ * Copyright 2025 KPG-TB
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package dev.projectenhanced
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 
-class EnhancedSpigotPlugin implements Plugin<Project>{
+class EnhancedSpigotPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        def ext = project.extensions.create("enhancedspigot",EnhancedSpigotExtension);
+        def ext = project.extensions.create("enhancedspigot", EnhancedSpigotExtension);
         ext.relocationPath.convention("")
         ext.excludeDependencies.convention(true)
         ext.importVault.convention(false);
         ext.importPapi.convention(false);
-        ext.commandsModule.convention("1.0.0-SNAPSHOT")
-        ext.configsModule.convention("1.0.0-SNAPSHOT")
-        ext.dataModule.convention("1.0.0-SNAPSHOT")
-        ext.itemsModule.convention("1.0.0-SNAPSHOT")
-        ext.localeModule.convention("1.0.0-SNAPSHOT")
-        ext.menusModule.convention("1.0.0-SNAPSHOT")
-        ext.pluginModule.convention("1.0.0-SNAPSHOT")
-        ext.utilsModule.convention("1.0.0-SNAPSHOT")
+        ext.commandsModule.convention("")
+        ext.configsModule.convention("")
+        ext.dataModule.convention("")
+        ext.itemsModule.convention("")
+        ext.localeModule.convention("")
+        ext.menusModule.convention("")
+        ext.pluginModule.convention("")
+        ext.utilsModule.convention("")
 
         project.getPluginManager().apply("io.freefair.lombok")
         project.getPluginManager().apply("com.gradleup.shadow")
@@ -36,9 +52,9 @@ class EnhancedSpigotPlugin implements Plugin<Project>{
                 mavenCentral()
                 mavenLocal()
                 maven { url "https://nexus.projectenhanced.dev/repository/maven-snapshots" }
-              //  if(!ext.menusModule.get().isEmpty()) maven {url 'https://repo.codemc.io/repository/maven-snapshots'}
-                if(ext.importVault.get()) maven { url 'https://jitpack.io' }
-                if(ext.importPapi.get()) maven {url = 'https://repo.extendedclip.com/releases/'}
+                if (!ext.menusModule.get().isEmpty()) maven { url 'https://repo.codemc.io/repository/maven-snapshots' }
+                if (ext.importVault.get()) maven { url 'https://jitpack.io' }
+                if (ext.importPapi.get()) maven { url = 'https://repo.extendedclip.com/releases/' }
             }
 
             project.dependencies {
@@ -51,8 +67,8 @@ class EnhancedSpigotPlugin implements Plugin<Project>{
                 if (!ext.itemsModule.get().isEmpty()) implementation "dev.projectenhanced:EnhancedSpigot-Items:" + ext.itemsModule.get()
                 if (!ext.pluginModule.get().isEmpty()) implementation "dev.projectenhanced:EnhancedSpigot-Plugin:" + ext.pluginModule.get()
 
-                if(ext.importVault.get()) compileOnly "com.github.MilkBowl:VaultAPI:1.7"
-                if(ext.importPapi.get()) compileOnly 'me.clip:placeholderapi:2.11.7'
+                if (ext.importVault.get()) compileOnly "com.github.MilkBowl:VaultAPI:1.7"
+                if (ext.importPapi.get()) compileOnly 'me.clip:placeholderapi:2.11.7'
             }
 
             project.tasks.withType(JavaCompile).configureEach {
@@ -68,7 +84,7 @@ class EnhancedSpigotPlugin implements Plugin<Project>{
 
                 )
 
-                if(ext.excludeDependencies.get()) {
+                if (ext.excludeDependencies.get()) {
                     exclude("com/j256/ormlite/**")
                     exclude("org/postgresql/**")
                     exclude("com/zaxxer/**")
