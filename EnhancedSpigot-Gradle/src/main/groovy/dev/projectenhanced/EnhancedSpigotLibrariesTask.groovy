b2f3c5603b551d832d0b465f07108936e993004d
KPGTB <kok.krzysztof@outlook.com>
2025-12-14 23:08:06 +0100

    1.0.2 plugin

--- EnhancedSpigot-Gradle/src/main/groovy/dev/projectenhanced/EnhancedSpigotLibrariesTask.groovy
+++ EnhancedSpigot-Gradle/src/main/groovy/dev/projectenhanced/EnhancedSpigotLibrariesTask.groovy
@@ -5,17 +5,18 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
-abstract class EnhancedSpigotLibrariesTask extends DefaultTask{
+abstract class EnhancedSpigotLibrariesTask extends DefaultTask {
     @Input
     abstract Property<String> getDataModule()
+
     @Input
     abstract Property<Boolean> getExcludeDependencies()
 
     @TaskAction
     void run() {
-        if(!getDataModule().get().isEmpty() && getExcludeDependencies().get()) {
+        if (!getDataModule().get().isEmpty() && getExcludeDependencies().get()) {
             println 'libraries:'
-            println '- com.j256.ormlite.ormlite-jdbc:6.1'
+            println '- com.j256.ormlite:ormlite-jdbc:6.1'
             println '- org.postgresql:postgresql:42.7.5'
             println '- com.zaxxer:HikariCP:4.0.3'
         } else {
